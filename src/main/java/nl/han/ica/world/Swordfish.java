0a5a975495c87d84635861501919e62d4f6a77be
Mustafa <info@codesimple.nl>
2017-10-11 00:44:42 +0200

    Name refactor & gitignore

---
+++ src/main/java/nl/han/ica/world/Swordfish.java
@@ -0,0 +1,40 @@
+package nl.han.ica.world;
+
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.SpriteObject;
+
+/**
+ * @author Ralph Niels
+ * Een zwaardvis is een spelobject dat zelfstandig
+ * door de wereld beweegt
+ */
+public class Swordfish extends SpriteObject {
+
+    private BeanWorld world;
+
+    /**
+     * Constructor
+     * @param world Referentie naar de wereld
+     */
+    public Swordfish(BeanWorld world) {
+        this(new Sprite("src/main/java/nl/han/ica/world/media/swordfish.png"));
+        this.world=world;
+    }
+
+    /**
+     * Maak een Swordfish aan met een sprite
+     * @param sprite De sprite die aan dit object gekoppeld moet worden
+     */
+    private Swordfish(Sprite sprite) {
+        super(sprite);
+        setxSpeed(-1);
+    }
+
+    @Override
+    public void update() {
+        if (getX()+getWidth()<=0) {
+            setX(world.getWidth());
+        }
+
+    }
+}
