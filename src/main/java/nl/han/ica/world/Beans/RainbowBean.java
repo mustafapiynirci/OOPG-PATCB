a4d7bbaeea26d3b2596c6e707f18c1380125bab9
Mustafa Piynirci <mustafapiynirci@hotmail.com>
2017-10-23 11:31:37 +0200

    Rainbowbean added

--- src/main/java/nl/han/ica/world/Beans/RainbowBean.java
+++ src/main/java/nl/han/ica/world/Beans/RainbowBean.java
@@ -1,4 +1,17 @@
-//package nl.han.ica.world.Beans;
-//
-//public class RainbowBean extends Bean {
-//}
+package nl.han.ica.world.Beans;
+
+import nl.han.ica.OOPDProcessingEngineHAN.Alarm.IAlarmListener;
+import nl.han.ica.world.BeanWorld;
+
+public class RainbowBean extends Bean implements IAlarmListener {
+
+    public RainbowBean(BeanWorld world, int beanSize) {
+        super(world, beanSize, "nl/han/ica/world/media/rainbowbean.png", 4);
+        setCurrentFrameIndex(0);
+    }
+
+    @Override
+    public void triggerAlarm(String alarmName) {
+
+    }
+}
