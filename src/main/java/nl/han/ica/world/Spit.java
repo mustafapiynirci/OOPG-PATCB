ca8939d27463ea3480dbf2b5c3f25e29348fc7a6
Mustafa Piynirci <mustafapiynirci@hotmail.com>
2017-10-26 16:30:08 +0200

    Mergeconflict fix

--- src/main/java/nl/han/ica/world/Spit.java
+++ src/main/java/nl/han/ica/world/Spit.java
@@ -1,7 +1,9 @@
 package nl.han.ica.world;
 
 import nl.han.ica.OOPDProcessingEngineHAN.Collision.ICollidableWithGameObjects;
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.AnimatedSpriteObject;
 import nl.han.ica.OOPDProcessingEngineHAN.Objects.GameObject;
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
 import nl.han.ica.world.Beans.Bean;
 import nl.han.ica.world.Beans.RainbowBean;
 import processing.core.PConstants;
@@ -9,61 +11,6 @@ import processing.core.PGraphics;
 
 import java.util.List;
 
-<<<<<<< HEAD
-public class Spit extends AnimatedSpriteObject implements ICollidableWithGameObjects {
-
-    private int spitSize;
-    private BeanWorld world;
-
-    private int thisX;
-    private int thisY;
-    private LookingSide side;
-
-    public Spit(BeanWorld world, int spitSize, LookingSide side) {
-        super(new Sprite("nl/han/ica/world/media/whiteCircle.png"), 1);
-        this.world = world;
-        for(GameObject g : world.getGameObjectItems()) {
-            if(g instanceof Pajaro) {
-                thisX = (int) g.getX();
-                thisY = (int) g.getY();
-            }
-        }
-        setHeight(spitSize);
-        setWidth(spitSize);
-        setSpeed(5);
-        this.spitSize = spitSize;
-        this.side = side;
-    }
-
-
-    @Override
-    public void gameObjectCollisionOccurred(List<GameObject> collidedGameObjects) {
-        for(GameObject g : collidedGameObjects) {
-            if(g instanceof Bean) {
-                world.setCurrentScore(world.getCurrentScore() + ((Bean) g).getScore());
-                if(g instanceof RainbowBean) {
-                    ((RainbowBean) g).popBean();
-                } else {
-                    world.deleteGameObject(g);
-                }
-                world.deleteGameObject(this);
-                world.refreshDasboardText();
-            }
-        }
-    }
-
-    @Override
-    public void update() {
-        if (getY() <= (0 - getHeight()) || getX() <= (0 - getWidth()) || getX() >= (world.width + getWidth())) {
-            world.deleteGameObject(this);
-        }
-        if(side == LookingSide.LEFT) {
-            setX(getX() - getSpeed());
-        } else if (side == LookingSide.RIGHT) {
-            setX(getX() + getSpeed());
-        }
-    }
-=======
 public class Spit extends GameObject implements ICollidableWithGameObjects {
 	
 	private BeanWorld world;
@@ -125,5 +72,4 @@ public class Spit extends GameObject implements ICollidableWithGameObjects {
 			world.deleteGameObject(this);
 		}
 	}
->>>>>>> master
 }
